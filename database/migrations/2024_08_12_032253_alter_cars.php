91d3ed9742544b6fcb2dfe698acaebba4fbbfe70
Mário Gustavo Kreibich <gukre92@gmail.com>
2024-08-12 01:47:00 -0300

    Add coluna tabela carros

---
+++ database/migrations/2024_08_12_032253_alter_cars.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('cars', function (Blueprint $table) {
+            $table->after('price', function (Blueprint $table) {
+                $table->dateTime('external_updated_at');
+            });
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('cars', function (Blueprint $table) {
+            $table->dropColumn('external_updated_at');
+        });
+    }
+};
